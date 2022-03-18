f5a46ad1d195e6855ba9ce51514b914102f62959
yamz-11 <wiratamajrz@gmail.com>
2022-03-18 11:21:26 +0700

    Add project files.

---
+++ Create_Table/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Create_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().CreateTable();
+        }
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+        }
+    }
+}
